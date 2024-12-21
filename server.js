c1515b4f8fba203e9ddf98ee27084884bb5654ea
missarii <143264692+missarii@users.noreply.github.com>
2024-12-21 14:47:35 +0530

    Update server.js MHZ2250

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/submit-form', async (req, res) => {
     await newUser.save();
 
     // Redirect on success
-    res.redirect('https://forms.gle/K4PTwTKXCjWToXNr5');
+    res.redirect('https://forms.gle/NEKUmE4s8egt6LwM8');
   } catch (error) {
     console.error('Error saving data to MongoDB:', error);
     res.status(500).send('<h3 style="color: red;">Server error. Please try again later.</h3>');
